29854d8bee67cc19258bd0c2ec612749d3fa0032
Emircan Gürbüz <125201557+grbzemir@users.noreply.github.com>
2024-01-06 17:15:43 +0300

    Adding and deleting data to the tree

--- VeriYapıları.c
+++ VeriYapıları.c
@@ -12,7 +12,7 @@ struct agac
 
 typedef struct agac node; // takma ad verdik!
 
-node *agacEkle(node *agac, int x) // int x ekleyeceğimiz değer!
+node *agacEkle(node *agac, int x) // int x ekleyeceÄŸimiz deÄŸer!
 
 {
     if (agac == NULL)
@@ -61,24 +61,107 @@ void Dolas(node *agac)
     
 }
 
+int maksimum(node *agac) 
+{
+
+    while(agac->SagBag != NULL) // saÄŸa gidiyoruz!
+    
+    {
+    	
+        agac = agac->SagBag;
+        
+    }
+
+    return agac->veri;
+
+
+}
+
+int minimum(node *agac)
+
+{
+    while(agac->SolBag !=NULL)
+
+    {
+        agac = agac->SolBag;
+    }
+
+    return agac->veri;
+}
+
+
+node *sil(node *agac,int silinen)
+{
+	if(agac==NULL)
+		return NULL;
+
+	if(agac->veri==silinen)
+    {
+
+		if(agac->SagBag==NULL&&agac->SolBag==NULL)
+
+			return NULL;
+
+			}
+
+			if(agac->SagBag!=NULL)
+
+            {
+				agac->veri=minimum(agac->SagBag);
+				agac->SagBag=sil(agac->SagBag,minimum(agac->SagBag));
+				return agac;
+
+			}
+
+			agac->veri=maksimum(agac->SolBag);
+			agac->SolBag=sil(agac->SolBag,maksimum(agac->SolBag));
+			return agac;
+
+   if(agac->SagBag <silinen)
+
+   {
+
+	agac->SagBag=sil(agac->SagBag,silinen);
+	return agac;
+
+   }
+
+   agac->SolBag=sil(agac->SolBag,silinen);
+   return agac;
+
+   }
+
+
 int main()
 
 {
     system("color 0a");
 
-    node *kok = NULL;
+    node *agac = NULL;
 
-    kok = agacEkle(kok, 10);
-    kok = agacEkle(kok, 20);
-    kok = agacEkle(kok, 30);
-    kok = agacEkle(kok, 40);
-    kok = agacEkle(kok, 50);
-    kok = agacEkle(kok, 60);
-    kok = agacEkle(kok, 70);
-    kok = agacEkle(kok, 80);
-    kok = agacEkle(kok, 90);
+    agac = agacEkle(agac, 10);
+    agac = agacEkle(agac, 20);
+    agac = agacEkle(agac, 30);
+    agac = agacEkle(agac, 40);
+    agac = agacEkle(agac, 50);
+    agac = agacEkle(agac, 60);
+    agac = agacEkle(agac, 70);
+    agac = agacEkle(agac, 80);
+    agac = agacEkle(agac, 90);
 
-    Dolas(kok);
+    printf("\n");
+    printf("Maksimum Deger: %d\n", maksimum(agac));
+    printf("Minimum Deger: %d\n", minimum(agac));   
+    agac = sil(agac, 10);
+    Dolas(agac);
+    printf("\n");
+    agac = sil(agac, 30);
+    Dolas(agac);
+    agac = sil(agac , 40);
+    printf("\n");
+    Dolas(agac);
+
+
 
     return 0;
     
